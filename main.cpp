4c65f4372286c60b964e01262424847a4efe5ecc
Md. Touhidul Islam <110239106+touhid0503@users.noreply.github.com>
2025-11-04 10:17:35 +0600

    Implement geometric transformations in main.cpp
    Added functions for scaling, rotation, reflection, and shearing transformations.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-///It will be update
 #include <GL/glut.h>
 #include<cmath>
 
@@ -44,6 +43,65 @@ void translation()
     glEnd();
 }
 
+void scaling()
+{
+    float x1 = 4.0f, y1 = 6.0f;
+    float x2 = 2.0f, y2 = 2.0f;
+    float x3 = 6.0f, y3 = 2.0f;
+
+    float Sx = 2.0f;
+    float Sy = 2.0/3.0f;
+
+    float x1_t = x1 * Sx, y1_t = y1 * Sy;
+    float x2_t = x2 * Sx, y2_t = y2 * Sy;
+    float x3_t = x3 * Sx, y3_t = y3 * Sy;
+
+
+    glColor3f(1, 0, 0);
+    glBegin(GL_LINES);
+        glVertex2f(x1, y1);
+        glVertex2f(x2, y2);
+
+        glVertex2f(x2, y2);
+        glVertex2f(x3, y3);
+
+        glVertex2f(x3, y3);
+        glVertex2f(x1, y1);
+    glEnd();
+
+
+    glColor3f(0, 1, 0);
+    glBegin(GL_LINES);
+        glVertex2f(x1_t, y1_t);
+        glVertex2f(x2_t, y2_t);
+
+        glVertex2f(x2_t, y2_t);
+        glVertex2f(x3_t, y3_t);
+
+        glVertex2f(x3_t, y3_t);
+        glVertex2f(x1_t, y1_t);
+    glEnd();
+}
+
+
+
+void dcd()
+{
+
+    glBegin(GL_POINTS);
+
+    for (int x = 0; x <= r; x++)
+    {
+        int y = (int)(sqrt(r * r - x * x));
+        glVertex2i(x, y);
+        glVertex2i(x, -y);
+        glVertex2i(-x, -y);
+        glVertex2i(-x, y);
+    }
+    glEnd();
+    glFlush();
+}
+
 void mcd()
 {
     int x=0;
@@ -80,21 +138,155 @@ void mcd()
     glFlush();
 }
 
-void dcd()
+void rotation()
 {
+    float x1 = 4.0f, y1 = 6.0f;
+    float x2 = 2.0f, y2 = 2.0f;
+    float x3 = 6.0f, y3 = 2.0f;
 
-    glBegin(GL_POINTS);
+    float theta=45;
+    float rad=theta*3.1416/180;
 
-    for (int x = 0; x <= r; x++)
-    {
-        int y = (int)(sqrt(r * r - x * x));
-        glVertex2i(x, y);
-        glVertex2i(x, -y);
-        glVertex2i(-x, -y);
-        glVertex2i(-x, y);
-    }
+    float x1_t = x1 * cos(rad)-y1*sin(rad), y1_t = y1 * cos(rad)+x1*sin(rad);
+    float x2_t = x2 * cos(rad)-y2*sin(rad), y2_t = y2 * cos(rad)+x2*sin(rad);
+    float x3_t = x3 * cos(rad)-y3*sin(rad), y3_t = y3 * cos(rad)+x3*sin(rad);
+
+
+    glColor3f(1, 0, 0);
+    glBegin(GL_LINES);
+    glVertex2f(x1, y1);
+    glVertex2f(x2, y2);
+
+    glVertex2f(x2, y2);
+    glVertex2f(x3, y3);
+
+    glVertex2f(x3, y3);
+    glVertex2f(x1, y1);
     glEnd();
-    glFlush();
+
+
+    glColor3f(0, 1, 0);
+    glBegin(GL_LINES);
+    glVertex2f(x1_t, y1_t);
+    glVertex2f(x2_t, y2_t);
+
+    glVertex2f(x2_t, y2_t);
+    glVertex2f(x3_t, y3_t);
+
+    glVertex2f(x3_t, y3_t);
+    glVertex2f(x1_t, y1_t);
+    glEnd();
+}
+
+void reflection()
+{
+    float x1 = 4.0f, y1 = 6.0f;
+    float x2 = 2.0f, y2 = 2.0f;
+    float x3 = 6.0f, y3 = 2.0f;
+
+
+
+    float x1_t = x1, y1_t = -y1 ;
+    float x2_t = x2, y2_t = -y2;
+    float x3_t = x3, y3_t = -y3;
+
+
+    float x11_t = -x1, y11_t = y1 ;
+    float x22_t = -x2, y22_t = y2;
+    float x33_t = -x3, y33_t = y3;
+
+
+
+    glColor3f(1, 0, 0);
+    glBegin(GL_LINES);
+    glVertex2f(x1, y1);
+    glVertex2f(x2, y2);
+
+    glVertex2f(x2, y2);
+    glVertex2f(x3, y3);
+
+    glVertex2f(x3, y3);
+    glVertex2f(x1, y1);
+    glEnd();
+
+
+    glColor3f(0, 1, 0);
+    glBegin(GL_LINES);
+    glVertex2f(x1_t, y1_t);
+    glVertex2f(x2_t, y2_t);
+
+    glVertex2f(x2_t, y2_t);
+    glVertex2f(x3_t, y3_t);
+
+    glVertex2f(x3_t, y3_t);
+    glVertex2f(x1_t, y1_t);
+    glEnd();
+
+    glBegin(GL_LINES);
+    glVertex2f(x11_t, y11_t);
+    glVertex2f(x22_t, y22_t);
+
+    glVertex2f(x22_t, y22_t);
+    glVertex2f(x33_t, y33_t);
+
+    glVertex2f(x33_t, y33_t);
+    glVertex2f(x11_t, y11_t);
+    glEnd();
+}
+void shearing()
+{
+    float x1 = 4.0f, y1 = 6.0f;
+    float x2 = 2.0f, y2 = 2.0f;
+    float x3 = 6.0f, y3 = 2.0f;
+
+    float shr=3;
+
+    float x1_t = x1+shr*y1, y1_t = y1 ;
+    float x2_t = x2+shr*y2, y2_t = y2;
+    float x3_t = x3+shr*y3, y3_t = y3;
+
+
+    float x11_t = x1, y11_t = y1+shr*x1 ;
+    float x22_t = x2, y22_t = y2+shr*x2;
+    float x33_t = x3, y33_t = y3+shr*x3;
+
+
+
+    glColor3f(1, 0, 0);
+    glBegin(GL_LINES);
+    glVertex2f(x1, y1);
+    glVertex2f(x2, y2);
+
+    glVertex2f(x2, y2);
+    glVertex2f(x3, y3);
+
+    glVertex2f(x3, y3);
+    glVertex2f(x1, y1);
+    glEnd();
+
+
+    glColor3f(0, 1, 0);
+    glBegin(GL_LINES);
+    glVertex2f(x1_t, y1_t);
+    glVertex2f(x2_t, y2_t);
+
+    glVertex2f(x2_t, y2_t);
+    glVertex2f(x3_t, y3_t);
+
+    glVertex2f(x3_t, y3_t);
+    glVertex2f(x1_t, y1_t);
+    glEnd();
+
+    glBegin(GL_LINES);
+    glVertex2f(x11_t, y11_t);
+    glVertex2f(x22_t, y22_t);
+
+    glVertex2f(x22_t, y22_t);
+    glVertex2f(x33_t, y33_t);
+
+    glVertex2f(x33_t, y33_t);
+    glVertex2f(x11_t, y11_t);
+    glEnd();
 }
 
 void display()
@@ -102,8 +294,11 @@ void display()
     glClear(GL_COLOR_BUFFER_BIT);
     //dcd(); //init
     //mcd(); //init
-    translation(); //init2
-
+    //translation(); //init2
+    //scaling(); //init2
+    //rotation(); //init2
+    //reflection(); //init2
+    //shearing(); //init2
     glutSwapBuffers();
 
 }
@@ -119,7 +314,7 @@ void init2()
 {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(-15, 15, -15, 15);
+    gluOrtho2D(-25, 25, -25, 25);
     glMatrixMode(GL_MODELVIEW);
 }
 int main(int argc, char** argv)
@@ -134,5 +329,4 @@ int main(int argc, char** argv)
     glutMainLoop();
     return 0;
 }
-
 
